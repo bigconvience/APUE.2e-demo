d9e7b5a7b209c3724e1127d8aa4d362302b8f893
bigconvience <jbp.130@163.com>
2015-02-09 22:42:04 +0800

    add makefile for quick_sort

--- quick_sort/check_quick_sort.c
+++ quick_sort/check_quick_sort.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#include "../src/quick_sort.h"
+#include "quick_sort.h"
 
 int main () {
     int a[] = {4, 65, 2, -31, 0, 99, 2, 83, 782, 1};
-    int expect[] = {-31, 0, 1, 2, 2, 4, 65, 83, 95, 782};
+    int expect[] = {-31, 0, 1, 2, 2, 4, 65, 83, 99, 782};
     int n = sizeof a / sizeof a[0];
     int i;
    
     quick_sort(a, n);
-    bool success = true;
+    int success = 1;
     for (i = 0; i < n; i++)
     {
 	    if (a[i] != expect[i])
 	    {
+		    success = 0;
 		    printf("test failed:at %d, expect:%d, src:%d\n", i, a[i], expect[i]);
 	    }
+    }
+    if (success)
+    {
+	    printf("quick sort test success\n");
     }
     return 0;
 }

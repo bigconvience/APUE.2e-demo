1037047224005dfe3a35e64d6e80567a5f0c888e
本朋 <benpeng.jbp@alibaba-inc.com>
2017-09-24 17:07:45 +0800

    add apue.3e

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include "apue.h"
-#include <sys/wait.h>
 
-#define	DEF_PAGER	"/usr/bin/more"		/* default pager program */
+
+#define offset(TYPE, MEMBER) ((int)&((TYPE *)0)->MEMBER)
+
+struct person {
+    int age;
+    char tal;
+    char *name;
+};
 
 int
 main(int argc, char *argv[])
 {
     printf("uid = %d, gid = %d, euid = %d, egid = %d\n", getuid(), getgid(), geteuid(), getegid());
+
+    printf("offset %d\n", offset(struct person, name));
     exit(0);
 }
